d34b592b65f76601bb9c6f72f6326c76ed34617a
Андрей Шамовцев <a.shamovtsev@cft.ru>
2019-02-05 13:01:00 +0700

    Создан пакет для моей первой программы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,8 @@
-public class helloWorld{
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
     public static void main(String args[]){
         System.out.println("Hello worlds!");
+        System.out.println(2.0+2);
     }
 }
