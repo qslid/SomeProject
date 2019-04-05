30c29a18c24e79ae01b7cb6539c666c506426c81
Andrey <procyonws@gmail.com>
2019-04-05 21:25:51 +0700

    Запуск браузера
    Добавлен запуск браузера с возможностью выбора из конструктора

--- address-book-web-tests/src/test/java/ru/pft/stqa/addressbook/appmanager/ApplicationManager.java
+++ address-book-web-tests/src/test/java/ru/pft/stqa/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.pft.stqa.addressbook.appmanager;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,9 +18,23 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private boolean acceptNextAlert = true;
     private StringBuffer verificationErrors = new StringBuffer();
+    private String browser;
+
+    public ApplicationManager(String firefox) {
+
+        browser = firefox;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        }
+        else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        } else if(browser.equals(BrowserType.IEXPLORE)){
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         wd.get("http://autotest.h1n.ru/addressbook/index.php");
 
